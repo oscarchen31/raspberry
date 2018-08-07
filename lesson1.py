1d85d6fb4799d15b7c950a225773c334c4d238bc
oscar chen <oscarchen31@gmail.com>
2018-08-07 14:40:04 +0800

    加了sort和sorted

--- lesson1.py
+++ lesson1.py
@@ -1,2 +1,27 @@
-for countdown in 5,4,3,2,1,"hey!":
-    print(countdown)
+#!usr/bin/python3.5
+#join
+
+marxes = ["Groucho", "Chico", "Harpo"];
+joinString = ", ".join(marxes);
+print(joinString);
+marxess = joinString.split(", ");
+print(marxess);
+
+
+#sort() sort the list itself
+#sorted() returns a sorted copy
+
+marxes = ["Groucho", "Chico", "Harpo"];
+sorted_marxes = sorted(marxes);
+print("sorted_marxes",sorted_marxes);
+
+print("marxes:",marxes);
+
+marxes.sort();
+print("marxes after sort:",marxes);
+
+numbers = [2, 1, 4.0, 3];
+numbers.sort();
+print("numbers:",numbers);
+numbers.sort(reverse=True);
+print("numbers reverse",numbers);
